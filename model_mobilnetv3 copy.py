a5e6f7387f9ea9f16e3fb01c12f56d7770ede090
byang12159 <92831530+byang12159@users.noreply.github.com>
2023-12-18 19:27:44 -0600

    changes

--- model_mobilnetv3 copy.py
+++ model_mobilnetv3 copy.py
@@ -1,29 +1,53 @@
+def train_mse_loss(teacher, student, train_loader, epochs, learning_rate, feature_map_weight, ce_loss_weight, device):
+    ce_loss = nn.CrossEntropyLoss()
+    mse_loss = nn.MSELoss()
+    optimizer = optim.Adam(student.parameters(), lr=learning_rate)
 
+    teacher.to(device)
+    student.to(device)
+    teacher.eval()  # Teacher set to evaluation mode
+    student.train() # Student to train mode
 
-from torchvision.io import read_image
-from torchvision.models.quantization import resnet50, ResNet50_QuantizedWeights
+    for epoch in range(epochs):
+        running_loss = 0.0
+        for inputs, labels in train_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
 
-img = read_image("office_downsample480_270.jpg")
+            optimizer.zero_grad()
 
-# Step 1: Initialize model with the best available weights
-weights = ResNet50_QuantizedWeights.DEFAULT
-model = resnet50(weights=weights, quantize=True)
-model.eval()
+            # Again ignore teacher logits
+            with torch.no_grad():
+                _, teacher_feature_map = teacher(inputs)
 
-# Step 2: Initialize the inference transforms
-preprocess = weights.transforms()
+            # Forward pass with the student model
+            student_logits, regressor_feature_map = student(inputs)
 
-# Step 3: Apply inference preprocessing transforms
-batch = preprocess(img).unsqueeze(0)
+            # Calculate the loss
+            hidden_rep_loss = mse_loss(regressor_feature_map, teacher_feature_map)
 
-started = time.time()
-# Step 4: Use the model and print the predicted category
-prediction = model(batch).squeeze(0).softmax(0)
-class_id = prediction.argmax().item()
-score = prediction[class_id].item()
-now = time.time()
-# do something with output ...
-print("time",now-started)
-category_name = weights.meta["categories"][class_id]
-print(f"{category_name}: {100 * score}%")
-       
+            # Calculate the true label loss
+            label_loss = ce_loss(student_logits, labels)
+
+            # Weighted sum of the two losses
+            loss = feature_map_weight * hidden_rep_loss + ce_loss_weight * label_loss
+
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+        print(f"Epoch {epoch+1}/{epochs}, Loss: {running_loss / len(train_loader)}")
+
+# Notice how our test function remains the same here with the one we used in our previous case. We only care about the actual outputs because we measure accuracy.
+
+# Initialize a ModifiedLightNNRegressor
+torch.manual_seed(42)
+modified_nn_light_reg = ModifiedLightNNRegressor(num_classes=10).to(device)
+
+# We do not have to train the modified deep network from scratch of course, we just load its weights from the trained instance
+modified_nn_deep_reg = ModifiedDeepNNRegressor(num_classes=10).to(device)
+modified_nn_deep_reg.load_state_dict(nn_deep.state_dict())
+
+# Train and test once again
+train_mse_loss(teacher=modified_nn_deep_reg, student=modified_nn_light_reg, train_loader=train_loader, epochs=10, learning_rate=0.001, feature_map_weight=0.25, ce_loss_weight=0.75, device=device)
+test_accuracy_light_ce_and_mse_loss = test_multiple_outputs(modified_nn_light_reg, test_loader, device)
